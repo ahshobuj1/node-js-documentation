b320b1077fa24834f282be8998d1ce389ff4c412
A H Shobuj <ahshobuj09@gmail.com>
2024-04-09 02:25:29 +0600

    Initial commit for node.js

---
+++ 01_Local_Module/main.js
@@ -0,0 +1,10 @@
+// const student = require(`./user.js`);
+
+// console.log(student.name());
+// console.log(student.age());
+// console.log(student.cgp());
+
+const {name, cgp} = require(`./user.js`);
+
+console.log(name());
+console.log(cgp());
---
+++ 01_Local_Module/user.js
@@ -0,0 +1,21 @@
+const name = () => {
+    return `A H Shobuj`;
+};
+
+const age = () => {
+    return `21`;
+};
+
+const cgp = () => {
+    return '2.52';
+};
+
+// exports.name = studentName;
+// exports.studentAge = studentAge;
+// exports.cgp = studentCgp;
+
+module.exports = {
+    name,
+    age,
+    cgp,
+};
